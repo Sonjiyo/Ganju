e207d3813bb458a38d4852ae8689dc902bb50cc3
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2024-04-01 11:35:47 +0900

    start

---
+++ ganjuProject/src/test/java/kr/ganjuproject/GanjuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ganjuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanjuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
