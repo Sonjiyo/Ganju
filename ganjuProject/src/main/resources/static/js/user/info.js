6e0f42004f5615f84a0827e5dcbd739012b462d5
PARK-Yunjae <dbswoql0712@gmail.com>
2024-04-04 15:27:49 +0900

    update
    user의 html css js 작업중

--- ganjuProject/src/main/resources/static/js/user/info.js
+++ ganjuProject/src/main/resources/static/js/user/info.js
@@ -1,5 +1,19 @@
 /* form 하기 전에 체크 */
 
-function infosubmit(form){
+function infosubmit(form) {
     form.submit();
 }
+
+const ocs = document.querySelectorAll('.option-contents-select');
+
+ocs.forEach(select => {
+    select.addEventListener('click', e => {
+        // 'this' 대신 'select' 사용
+        const input = select.querySelector('input[type=checkbox], input[type=radio]');
+        if (e.target !== input) {
+            input.checked = !input.checked;
+            // 라디오 버튼의 경우, 다른 라디오 버튼의 상태 변경을 위해 이벤트를 발생시킵니다.
+            input.dispatchEvent(new Event('change'));
+        }
+    });
+});
