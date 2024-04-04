aeb431a4abdb4cb2b9a0a0ae9bf1a6efe1d74ed3
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2024-04-04 12:13:20 +0900

    admin 페이지 합치기

--- ganjuProject/src/main/java/kr/ganjuproject/controller/ManagerController.java
+++ ganjuProject/src/main/java/kr/ganjuproject/controller/ManagerController.java
@@ -1,12 +1,41 @@
 package kr.ganjuproject.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 @RequestMapping("/manager")
+@RequiredArgsConstructor
 public class ManagerController {
 
+    @GetMapping("join")
+    public String join(){
+        return "manager/join";
+    }
+    @GetMapping("joinVerification")
+    public String joinVerification(){
+        return "manager/joinVerification";
+    }
+
+    @GetMapping("joinRestaurant")
+    public String joinRestaurant(){
+        return "manager/joinRestaurant";
+    }
+    @GetMapping("joinSuccess")
+    public String joinSuccess(){
+        return "manager/joinSuccess";
+    }
+
+    @GetMapping("order")
+    public String order(){
+        return "manager/order";
+    }
+    @GetMapping("sales")
+    public String salesr(){
+        return "manager/sales";
+    }
 }
