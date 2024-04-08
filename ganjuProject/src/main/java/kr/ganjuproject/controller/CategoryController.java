5209f09fde0a6669ddc5c5d9f11ec6d8d6cfc369
kyungjae-Y <yoonkj2008@gmail.com>
2024-04-08 12:45:12 +0900

    삭제, 추가
    삭제는 완료, 추가는 db 에 들어가지만 화면에 보이지 않음 .. 수정 중

--- ganjuProject/src/main/java/kr/ganjuproject/controller/CategoryController.java
+++ ganjuProject/src/main/java/kr/ganjuproject/controller/CategoryController.java
@@ -9,12 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 @Slf4j
@@ -23,6 +21,7 @@ import java.util.List;
 public class CategoryController {
     private final MenuOptionService menuOptionService;
     private final MenuService menuService;
+
     @GetMapping("main")
     public String menuCategory(Model model) {
         List<Menu> menus = menuService.getList();
@@ -31,17 +30,31 @@ public class CategoryController {
     }
 
     @GetMapping("add")
-    public String addOptionForm() {
+    public String addMenuForm(Model model) {
+        List<Menu> menus = menuService.getList();
+        model.addAttribute("menus", menus);
         return "manager/addMenu";
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addMenu(@RequestBody Menu menu) {
+        try {
+            menuService.add(menu);
+            return ResponseEntity.ok().body("메뉴가 성공적으로 등록되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("메뉴 등록에 실패하였습니다.");
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteMenu(@PathVariable Long id) {
         try {
             menuService.delete(id);
-            return ResponseEntity.ok().body("메뉴가 삭제되었습니다");
+            return ResponseEntity.ok().body("메뉴(ID : " + id + ")가 삭제되었습니다");
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID에 해당하는 메뉴를 찾을 수 없습니다");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("메뉴 삭제 중 오류가 발생했습니다");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("메뉴 삭제 중 오류가 발생했습니다 : " + e.getMessage());
         }
     }
 
