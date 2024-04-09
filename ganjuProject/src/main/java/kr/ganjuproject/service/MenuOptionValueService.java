9399103588c1c62ae073c97648f08b8665b4f16a
PARK-Yunjae <dbswoql0712@gmail.com>
2024-04-09 16:56:03 +0900

    up
    up

--- ganjuProject/src/main/java/kr/ganjuproject/service/MenuOptionValueService.java
+++ ganjuProject/src/main/java/kr/ganjuproject/service/MenuOptionValueService.java
@@ -1,13 +1,22 @@
 package kr.ganjuproject.service;
 
+import kr.ganjuproject.entity.MenuOption;
+import kr.ganjuproject.entity.MenuOptionValue;
 import kr.ganjuproject.repository.MenuOptionValueRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MenuOptionValueService {
     private final MenuOptionValueRepository menuOptionValueRepository;
+
+    public List<MenuOptionValue> findByMenuOptionId(Long menuOptionId){
+        return menuOptionValueRepository.findByMenuOptionId(menuOptionId);
+    }
+
 }
