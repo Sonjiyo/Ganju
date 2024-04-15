86962b456986c6d73f77e1558b3f3550b736b73b
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2024-04-15 14:07:36 +0900

    order리스트 받아오기

--- ganjuProject/src/main/java/kr/ganjuproject/repository/OrdersRepository.java
+++ ganjuProject/src/main/java/kr/ganjuproject/repository/OrdersRepository.java
@@ -2,11 +2,16 @@ package kr.ganjuproject.repository;
 
 import kr.ganjuproject.entity.Orders;
 import kr.ganjuproject.entity.Restaurant;
+import kr.ganjuproject.entity.RoleOrders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
     List<Orders> findByRestaurant(Restaurant restaurant);
+    List<Orders> findByRestaurantAndRegDateBetween(Restaurant restaurant, LocalDateTime startDate, LocalDateTime endDate);
+    List<Orders> findByRestaurantAndDivisionNotAndRegDateBetween(Restaurant restaurant, RoleOrders divisionToExclude, LocalDateTime startDate, LocalDateTime endDate);
+    List<Orders> findByRestaurantAndDivisionAndRegDateBetween(Restaurant restaurant, RoleOrders division, LocalDateTime startDate, LocalDateTime endDate);
 }
